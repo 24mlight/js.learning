9b76224a9907d9ad9a9259d9690abd379018bcad
24mlight <1506072192@qq.com>
2024-01-04 23:00:02 +0800

    upload

---
+++ src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)"
+      :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Tasks.vue' // from child 
+
+export default {
+  name: 'Tasks',
+  props: { 				// to be dilivered to father 
+    tasks: Array,
+  },
+  components: { 		// from child 
+    Task,
+  },
+  emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Version 1.0.0</h3>
+  <router-link to="/">Go Back</router-link> <!-- to="/" meand go to homepage -->
+</template>
+
+<script>
+export default {
+  name: 'About',
+  inheritAttrs: false, // disable 'non-props' warning
+};
+</script>
